603417d22b850c24cdbe33322e487b54ca3b8eed
CRE <crehmann@users.noreply.github.com>
2017-09-10 19:17:55 +0200

    Added initial project with observable ListViewAdapter and ViewCache

---
+++ DroidHelpers/DroidHelpers/ListView/ObservableListViewAdapter.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Views;
+using Android.Widget;
+using System.Collections.Specialized;
+
+namespace DroidHelpers.ListView
+{
+    /// <summary>
+    /// A <see cref="BaseAdapter{T}"/> that can be used with an Android ListView. If the <see cref="ItemSource"/> is
+    /// implementing <see cref="INotifyCollectionChanged"/>, than it will subscribe for collection changes.
+    /// </summary>
+    /// <typeparam name="T">The type of the items contained in the <see cref="ItemSource"/>.</typeparam>
+    public abstract class ObservableListViewAdapter<T> : BaseAdapter<T>
+    {
+        private readonly IList<Android.Views.View> _views = new List<Android.Views.View>();
+        private IList<T> _itemSource;
+        private INotifyCollectionChanged _observableCollection;
+
+        /// <summary>
+        /// The item source. If it implements INotifyCollectionChanged 
+        /// then the adapter will subscribe for collection changes.
+        /// </summary>
+        public IList<T> ItemSource
+        {
+            get
+            {
+                return _itemSource;
+            }
+            set
+            {
+                if (Equals(_itemSource, value)) return;
+
+                if (_observableCollection != null)
+                {
+                    _observableCollection.CollectionChanged -= NotifierCollectionChanged;
+                }
+
+                _itemSource = value;
+                _observableCollection = _itemSource as INotifyCollectionChanged;
+
+                if (_observableCollection == null) return;
+                _observableCollection.CollectionChanged += NotifierCollectionChanged;
+            }
+        }
+
+        /// <summary>
+        /// Returns the item at the specific position of the <see cref="ItemSource"/>.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <returns></returns>
+        public override T this[int position] => ItemSource[position];
+
+        /// <summary>
+        /// Returns the item count of the <see cref="ItemSource"/>.
+        /// </summary>
+        public override int Count => ItemSource?.Count ?? 0;
+
+        /// <summary>
+        /// Returns the item id for a specific position.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public override long GetItemId(int position) => 0L;
+
+        public sealed override Android.Views.View GetView(int position, Android.Views.View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+            if(view == null)
+            {
+                view = CreateView(position, parent);
+                view.Tag = new ViewHolder<T>(view);
+                _views.Add(view);
+            }
+
+            Bind(position, (ViewHolder<T>)view.Tag);
+            return view;
+        }
+
+        /// <summary>
+        /// Creates a view for the specific position
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="parent">The parent view.</param>
+        /// <returns></returns>
+        protected abstract Android.Views.View CreateView(int position, ViewGroup parent);
+
+        /// <summary>
+        /// Bind the view holder for an item at a specific position.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="viewHolder">The ViewHolder.</param>
+        protected abstract void Bind(int position, ViewHolder<T> viewHolder);
+
+        protected virtual void NotifierCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            NotifyDataSetChanged();
+        }
+
+        /// <summary>
+        /// Will dispose all views and ViewHolders. 
+        /// The adapter will unsubscribe itself from any INotifyCollectionChanged event.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (!disposing) return;
+
+            ItemSource = null;
+
+            foreach (var itemView in _views)
+            {
+                itemView.Tag?.Dispose();
+                itemView.Dispose();
+            }
+
+            _views.Clear();
+            Dispose();
+        }
+
+        /// <summary>
+        /// ViewHolder to cache views from an item view.
+        /// </summary>
+        /// <typeparam name="TItem"></typeparam>
+        public class ViewHolder<TItem> : Java.Lang.Object
+        {
+            public ViewHolder(Android.Views.View view)
+            {
+                if (view == null) throw new ArgumentNullException(nameof(view));
+                Views = new View.ViewCache(view);
+            }
+
+            public View.ViewCache Views { get; }
+
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+                if (!disposing) return;
+                Views.Dispose();
+                Dispose();
+            }
+        }
+    }
+}
---
+++ DroidHelpers/DroidHelpers/View/ViewCache.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+
+namespace DroidHelpers.View
+{
+    /// <summary>
+    /// ViewCache caches all views that are retrieved by id from an activity or view.
+    /// It helps you to dispose all views, that were requested.
+    /// </summary>
+    public class ViewCache : IDisposable
+    {
+        private readonly Dictionary<int, Android.Views.View> _cache = new Dictionary<int, Android.Views.View>();
+        private Activity _activityContext;
+        private Android.Views.View _viewContext;
+
+        /// <summary>
+        /// Creates a new ViewCache.
+        /// </summary>
+        /// <param name="host">The host activity which is used to find views by id.</param>
+        public ViewCache(Activity host)
+        {
+            _activityContext = host ?? throw new ArgumentNullException(nameof(host));
+        }
+
+        /// <summary>
+        /// Creates a new ViewCache.
+        /// </summary>
+        /// <param name="host">The host view which is used to find views by id.</param>
+        public ViewCache(Android.Views.View host)
+        {
+            _viewContext = host ?? throw new ArgumentNullException(nameof(host));
+        }
+
+        /// <summary>
+        /// Returns either a cached view or tries to find it by id.
+        /// </summary>
+        /// <typeparam name="T">The view type.</typeparam>
+        /// <param name="id">The id of the view.</param>
+        /// <returns></returns>
+        public T GetView<T>(int id) where T : Android.Views.View
+        {
+            if (!_cache.ContainsKey(id))
+            {
+                _cache.Add(id, FindViewById<T>(id));
+            }
+
+            return (T)_cache[id];
+        }
+
+        private T FindViewById<T>(int id) where T : Android.Views.View
+            => _activityContext?.FindViewById<T>(id)
+            ?? _viewContext.FindViewById<T>(id);
+
+        /// <summary>
+        /// Disposing all cached views. The host activity or host view will not be disposed.
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var item in _cache.Values)
+            {
+                item.Dispose();
+            }
+            _cache.Clear();
+            _activityContext = null;
+            _viewContext = null;
+        }
+    }
+}
